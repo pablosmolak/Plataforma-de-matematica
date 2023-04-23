e923d5018751f2e2f15037aaf83bb1c7d13b7142
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-04-23 19:32:18 -0400

    rota cursos

---
+++ src/models/Cursos.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate";
+import rotas from "./Rota";
+
+const cursoSchema = new mongoose.Schema(
+    [{
+        modulo: {type: String, required: true, trim: true},
+        nivel: {type: String, required: true, trim: true},
+        professor: [{
+          nome: {type: String, required: true, trim: true},
+        }],
+        aula: [{
+            orientacao: {type: String, required: true, trim: true},
+            videos: [{
+                nomeArquivo: {type: String, required: true, trim: true},
+                descricao: {type: String, required: true, trim: true},
+                data: {type: Date}
+            }],
+                arquivos: [{
+                nomeArquivo: {type: String, required: true, trim: true},
+                descricao: {type: String, required: true, trim: true},
+                data: {type: Date}
+            }],
+            comentarios: {type: String, required: true, trim: true},
+        }],
+
+        ativo: {type: Boolean, required: true}
+    }]
+);
+
+cursoSchema.plugin(mongoosePaginate);
+
+const rotas = mongoose.model('curso', cursoSchema);
+
+export default rotas;
