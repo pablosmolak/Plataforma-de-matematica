b315848d70844617def2eaaa4e563850ca883bce
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-04-27 16:00:06 -0400

    finalizando a model usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -5,12 +5,10 @@ const usuariosSchema = new mongoose.Schema(
 
     {
         nome: {type: String, required: true, trim: true, minlength: 3, maxlength: 200},
-        email: {type: String, required: true, trim: true, minlength: 3},
+        email: {type: String, required: true, trim: true, minlength: 3, unique: true},
+        senha : {type: String, required: true, minlength: 8},
         telefone: {type: String, required: true, trim: true},
-        senha : {type: String, required: true,},
         link_foto: { type: String, trim: true },
-        ativo: {type: Boolean},
-        rota: {type: String, required: true, trim: true},
         ativo: {type: Boolean, required: true, default: true},
         rotas: [
             {
