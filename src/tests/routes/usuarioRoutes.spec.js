5ccb32a173540c085ebf5fff0ad6db8e5777ef85
pablosmolak <pablosmolak2004@hotmail.com>
2023-05-26 22:27:51 -0400

    criando teste pra rota post usuario

--- src/tests/routes/usuarioRoutes.spec.js
+++ src/tests/routes/usuarioRoutes.spec.js
@@ -39,6 +39,14 @@ describe('/GET/ID em Usuarios', () =>{
         expect(dados._body.nome).toEqual('Dev oliveira');
 
     })
+    it("Deve retornar erro de ID invalido", async () => {
+        const dados = await request(app)
+        .get('/usuarios/647018d44693478cff31b66')
+        .set('accept', 'aplication/json')
+        .expect('content-type', /json/)
+        .expect(400);
+        expect(dados._body.message).toEqual('ID invalido ou não encontrado');
+    })
 })
 
 describe ('/POST em Usuarios', () => {
@@ -57,4 +65,33 @@ describe ('/POST em Usuarios', () => {
         const idPessoa = dados._body._id;
         
     });
+
+    it("Deve retornar erro de email ja cadastrado", async () =>{
+        const dados = await request(app)
+        .post('/usuarios')
+        .set('Accept', 'aplication/json')
+        .send({
+            nome: 'pablo Gabriel',
+            user: 'pablo21gabriel',
+            email: 'pablo21@gmail.com',
+            senha: '12325554',
+            telefone: '984227163'
+        })
+        .expect(422)
+    })
+
+    it("Deve retornar erro de User Name já cadastrado", async () =>{
+        const dados = await request(app)
+        .post('/usuarios')
+        .set('Accept', 'aplication/json')
+        .send({
+            nome: 'pablo Gabriel',
+            user: 'pablo21gabriel',
+            email: 'pablo211@gmail.com',
+            senha: '12325554',
+            telefone: '984227163'
+        })
+        .expect(422)
+        expect(dados._body.message).toEqual('Usuario já cadastrado!')
+    })
 });
