96066e30eec4d79664361e8001e7e5fb34a6a354
pablosmolak <pablosmolak2004@hotmail.com>
2023-06-07 22:15:18 -0400

    iniciando teste de unidade

---
+++ src/tests/models/usuarioModels.spec.js
@@ -0,0 +1,17 @@
+import {describe, expect, it, jest, beforeEach, afterAll, afterEach } from '@jest/globals'
+import Usuario from "../../models/Usuario.js"
+import UsuarioController from '../../controllers/UsuarioController.js'
+
+const usuarioFactory = {
+
+}
+
+describe("Deve retornar testes de unidade de pessoa", () =>{
+
+    afterEach(() => jest.clearAllMocks())
+
+    const objetoUsuario ={
+        nome: 'Pablo Smolak'
+        
+    }
+})
